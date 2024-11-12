959501e2ae2717ed73a265b330c945892c67dc69
Rubén Guadalupe Zavala Castañón <rubengpe.zavala@gmail.com>
2024-11-12 14:19:17 -0600

    local storage and componetize

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,34 @@ import { Square } from './components/Square'
 import { TURNS } from './constants'
 import { checkWinnerFrom, checkEndGame } from './logic/board'
 import { WinnerModal } from './components/WinnerModal'
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index'
 import './App.css'
 
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
+  console.log('render')
+  //no podemos leer el local sotare y condicionar el estado inicial
+  //los hooks sonn lo primero que reconoce react asi no podemos inmutarlo
+  //lo que podemos ahcer es dentro del use state usar una funciona anomima
+  //y que esta haga la evaluacion que necesitamos para saber si usaremos el localStorage
+  //o el estado defaul inicial (vacio)
+  const [board, setBoard] = useState(() => {
+    console.log('inicializar el etado del board')
+    //si sacamops este boardFromStorage del useState en cada render leera el localStorage y esto es LENTO
+    const boardFromStorage = window.localStorage.getItem('board')
+
+    if(boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+
+    //check if is null or undefined using ternary operator
+    //return boardFromStorage? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
   /* useState(['x','x','x','o','o','x','x','o','o']) */
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   console.log(board)
 
   //null no hay ganador, false es que hay un empate
@@ -21,6 +41,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   const updateBoard = (index) => {
@@ -44,6 +66,12 @@ function App() {
     //cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    //guardar aqui partida 
+    saveGameToStorage({
+      board : newBoard, 
+      turn: newTurn
+    })
 
     const newWinner = checkWinnerFrom(newBoard)
     if(newWinner) {
