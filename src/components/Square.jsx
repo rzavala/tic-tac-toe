0c0e3f7f8f3c9e36948bc051fdb1db09e7ab17d3
Rubén Guadalupe Zavala Castañón <rubengpe.zavala@gmail.com>
2024-11-12 14:38:21 -0600

    add standard for eslint

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,10 +1,10 @@
 export const Square = ({children, isSelected, updateBoard, index}) => {
     const className  = `square ${isSelected ? 'is-selected' : ''}`
-  
+
     const handleClick = () => {
       updateBoard(index)
     }
-  
+
     return (
        <div onClick={handleClick} className={className}>
         {children}
--- src/logic/storage/index.jsx
+++ src/logic/storage/index.jsx
@@ -3,7 +3,7 @@ export const saveGameToStorage = ({ board, turn }) => {
     window.localStorage.setItem('board', JSON.stringify(board))
     window.localStorage.setItem('turn', turn)
   }
-  
+
   export const resetGameStorage = () => {
     window.localStorage.removeItem('board')
     window.localStorage.removeItem('turn')
